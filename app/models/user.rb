6ff018d67b903cda545a139be21623c0f9cee2af
jonathan justman <jojo.justman@gmail.com>
2021-08-09 11:19:19 +0200

    desactivation mailer2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  after_create :welcome_send
+  # after_create :welcome_send
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
-  
+  # def welcome_send
+  #   UserMailer.welcome_email(self).deliver_now
+  # end
+
 end
